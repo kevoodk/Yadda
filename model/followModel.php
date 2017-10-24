d170eb27093cbe537b704ce7c3a20fd39c6747a1
Kevin <kevsmultimedia@gmail.com>
2017-10-24 10:08:13 +0200

    edited user model

--- model/followModel.php
+++ model/followModel.php
@@ -1,36 +1,3 @@
 <?php
 require_once 'model.php';
 
-class User extends model {
-    public $uid;
-    public $username;
-    public $forname;
-    public $picture;
-    public $rank;
-    public $active;
-
-    public function getYId(){
-        return $this->uid;
-    }
-
-    public function getUsername(){
-        return $this->username;
-    }
-
-    public function getForname(){
-        return $this->forname;
-    }
-
-    public function getPicture(){
-        return $this->picture;
-    }
-
-    public function getRank(){
-        return $this->rank;
-    }
-
-    public function getActive(){
-        return $this->active;
-    }
-
-}
